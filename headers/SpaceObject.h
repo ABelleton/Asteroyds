c7b8ee077fb5aacdbef0d3c239832affd79a7393
noe-morel <120017007+noe-morel@users.noreply.github.com>
2023-01-05 12:09:46 +0100

    ajout ID
    rajout d'un système d'ID pour les spaceobject afin de régler les conflits d'ordre de déplacement

--- headers/SpaceObject.h
+++ headers/SpaceObject.h
@@ -5,16 +5,21 @@
 
 class SpaceObject:Token{
     private:
-    bool[3] astType;
-    static int[3] diceValues;
+        bool[3] astType;
+        int idSpaceObject;
+        static int idEnum;
 
     public:
+        static int[3] diceValues;
+
         SpaceObject();
         SpaceObject(int x, int y);
         SpaceObject(bool rouge, bool blanc, bool bleu);
         SpaceObject(int x, int y, bool rouge, bool blanc, bool bleu);
+        getID() { return idSpaceObject; }
 
-    moveToken(int direction, Grid g);
+        moveToken(int direction, Grid g);
 }
 
 SpaceObject::diceValues = { 0,0,0 };
+SpaceObject::idEnum = 0;
