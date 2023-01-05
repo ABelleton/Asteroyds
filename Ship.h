7f1336ba704efcc6791b8d6e7c6b024dd8b0ba55
Maël Chakma <maelchakma@gmail.com>
2023-01-05 08:55:00 +0100

    Début fonctions

--- Ship.h
+++ Ship.h
@@ -9,7 +9,9 @@ class Ship : public Token {
 	int hitPoints;
 	bool[4] reachedDoors;
 public:
-	//Constructors
-	Ship(int id, int hp);
+	//Constructeurs
+	Ship(int id, int hp) { idShip = id; hitPoints = hp; }
 
+	//Fonctions virtuelles
+	virtual
 };
