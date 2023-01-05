2261d24466e1705eb80c916c3283cccad12f5842
noe-morel <120017007+noe-morel@users.noreply.github.com>
2023-01-05 09:16:31 +0100

    Ajout des constructeurs
    Ajout de 4 constructeurs pour la classe SpaceObject :+1:
    -par defaut
    -demandant les coordonnées
    -demandant les couleurs
    -demandant coordonnées et couleurs

--- SpaceObject.cpp
+++ SpaceObject.cpp
@@ -3,3 +3,19 @@
 SpaceObject::moveToken(int direction, Grid g){
     
 }
+
+SpaceObject::SpaceObject() : Token() {
+    astType = { false, false, false };
+}
+
+SpaceObject::SpaceObject(int x, int y) : Token(x, y) {
+    astType = { false, false, false };    
+}
+
+SpaceObject::SpaceObject(bool rouge, bool blanc, bool bleu) : Token() {
+    astType = { rouge, blanc, bleu };    
+}
+
+SpaceObject::SpaceObject(int x, int y, bool rouge, bool blanc, bool bleu) : Token(x, y) {
+        astType = { rouge, blanc, bleu };
+}
--- SpaceObject.h
+++ SpaceObject.h
@@ -9,5 +9,12 @@ class SpaceObject:Token{
     static int[3] diceValues;
 
     public:
+        SpaceObject();
+        SpaceObject(int x, int y);
+        SpaceObject(bool rouge, bool blanc, bool bleu);
+        SpaceObject(int x, int y, bool rouge, bool blanc, bool bleu);
+
     moveToken(int direction, Grid g);
 }
+
+SpaceObject::diceValues = { 0,0,0 };
